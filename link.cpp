1deddb2048a33a9d605191a37e60af6f7ae52a97
shubhams719 <shubhams719@gmail.com>
2018-05-12 16:50:24 +0530

    some change

--- link.cpp
+++ link.cpp
@@ -38,22 +38,15 @@ void pop(Node*& head)
 	return;
 }
 
-void print(Node* head)
+void print( Node* head)
 {
-	/*Node* temp = head;
+	Node* temp = head;
 	while(temp != NULL)
 	{
 		std::cout << temp->data << " ";
 		temp= temp->next;
-	}*/
-	std::cout <<"\n";
-	if(head == NULL)
-		std::cout << "linked list is empty";
-	while(head != NULL)
-	{
-		std::cout << head->data << " ";
-		head= head->next;
 	}
+	std::cout <<"\n";
 	return;
 }
 
@@ -142,11 +135,107 @@ void Delete(Node*& head)
 	return;
 }
 
+void frontbacksplit(Node* source,Node*& a,Node*& b)
+{
+	Node* fast= source;
+	Node* slow = source;
+
+	while(fast->next != NULL && fast != NULL && fast->next->next != NULL)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	a = source;
+	b = slow->next;
+	slow->next = NULL;
+}
+
+Node* SortedMerge(Node* a, Node* b)
+{
+	Node* result = NULL;
+	if(a == NULL)
+		return(b);
+	if(b==NULL)
+		return(a);
+	if(a->data > b->data)
+	{
+		result = b;
+		result->next = SortedMerge(a,b->next);
+	}
+	if(a->data < b->data)
+	{
+		result = a;
+		result->next = SortedMerge(a->next,b);
+	}
+	return(result);
+}
+
+void Merge(Node*& head)
+{
+	Node* source = head;
+	Node* a = NULL;
+	Node* b = NULL;
+	if(head == NULL || head->next == NULL)
+		return ;
+	frontbacksplit(source,a,b);
+	Merge(a);
+	Merge(b);
+	head = SortedMerge(a,b);
+}
+
+void Segragate(Node*& head)
+{
+	Node* curr = head;
+	Node* prev = NULL;
+	Node* tail = head;
+	int count = 0;
+	int len = 0;
+	while(tail->next != NULL)
+	{
+		tail = tail->next;
+		len++;
+	}
+
+		print(head);
+	while(head != NULL && ((head->data % 2) ==0) && count < len)
+	{
+		Node* new_tail = GetNode(curr->data);
+		tail->next = new_tail;
+		tail = new_tail;
+		prev = curr->next;
+		delete(curr);
+		curr = prev;
+		head = head->next;
+		count++;
+		print(head);
+	}
+	
+	while(curr != NULL && count < len)
+	{
+		if((curr->data % 2) != 0)
+		{
+			prev = curr;
+			curr = curr->next;
+		}
+		else
+		{
+			Node* new_tail = GetNode(curr->data);
+			tail->next = new_tail;
+			tail = new_tail;
+			prev->next = curr->next;
+			delete(curr);
+			curr = prev->next;
+		}
+		count++;
+	}
+}
 
 
 
 
+			
 
+		
 
 
 
